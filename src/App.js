8674ea28ba355517ba840ec28ad4ca1118f4519f
Mounika <bojjamounika8@gmail.com>
2021-12-28 18:00:55 +0530

    style

---
+++ src/App.js
@@ -0,0 +1,43 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+//function App() {
+  //return (
+    //<div className="App">
+     // <header className="App-header">
+      //  <img src={logo} className="App-logo" alt="logo" />
+       // <p>
+         // Edit <code>src/App.js</code> and save to reload.
+        //</p>
+        //<a
+         // className="App-link"
+          //href="https://reactjs.org"
+         // target="_blank"
+         // rel="noopener noreferrer"
+       // >
+         // Learn React
+        //</a>
+      //</header>
+    //</div>
+  //);
+//}
+
+//export default App;
+
+import React, { Component } from 'react';
+
+class App extends Component{
+  render(){
+    var myStyle = {
+      fontsize: 80,
+      fontfamily: 'Courier',
+      color: 'blue'
+    }
+    return (
+      <div>
+        <h1 style = {myStyle}> React Style-Element</h1>
+      </div>
+    );
+  }
+}
+export default App;
